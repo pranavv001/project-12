d91dad5c028e9d026c323965aec3fe44aead54e6
pranavv001 <85962191+pranavv001@users.noreply.github.com>
2021-07-22 12:03:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 var garden,rabbit;
 var gardenImg,rabbitImg;
+var apples, applesImg;
+var select_sprites 
+var orangeLeaf,orangeLeafImg
 
 function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
+  applesImg = loadImage("apple.png")
+  orangeLeafImg = loadImage("leaf.png")
 }
 
 function setup(){
@@ -18,32 +23,36 @@ garden.addImage(gardenImg);
 rabbit = createSprite(180,340,30,30);
 rabbit.scale =0.09;
 rabbit.addImage(rabbitImg);
+
 }
 
 
 function draw() {
   background(0);
-  
+  rabbit.x = mouseX
   edges= createEdgeSprites();
   rabbit.collide(edges);
 
   drawSprites();
+  
+  select_sprites = Math.round(random(1.2));
 
   if (frameCount % 80 === 0) {
     if (select_sprites === 1) {
-      createApples()
+      createAppales()
     }
      else {
     createLeaves()
   }
 }
-
+}
 function createApples() {
   apples = createSprite(random(50, 350), 40, 10, 10)
   apples.addImage(applesImg);
   apples.velocityY = 2
   apples.scale = 0.1
 }
+
 function createLeaves() {
   orangeLeaf = createSprite(random(50,350), 40, 10, 10)
   orangeLeaf.addImage(orangeLeafImg);
